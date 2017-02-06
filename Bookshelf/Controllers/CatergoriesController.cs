6be1b1bed0474e3fbd48d4f1b3205825720f2fc3
Denisa Iacob <denisaiacob18@gmail.com>
2017-02-06 15:09:47 -0500

    added books

---
+++ Bookshelf/Controllers/CatergoriesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Bookshelf.Models;
+
+namespace Bookshelf.Controllers
+{
+    public class CatergoriesController : Controller
+    {
+        private BookshelfContext db = new BookshelfContext();
+
+        // GET: Catergories
+        public ActionResult Index()
+        {
+            return View(db.Catergories.ToList());
+        }
+
+        // GET: Catergories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Catergory catergory = db.Catergories.Find(id);
+            if (catergory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(catergory);
+        }
+
+        // GET: Catergories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Catergories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] Catergory catergory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Catergories.Add(catergory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(catergory);
+        }
+
+        // GET: Catergories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Catergory catergory = db.Catergories.Find(id);
+            if (catergory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(catergory);
+        }
+
+        // POST: Catergories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] Catergory catergory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(catergory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(catergory);
+        }
+
+        // GET: Catergories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Catergory catergory = db.Catergories.Find(id);
+            if (catergory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(catergory);
+        }
+
+        // POST: Catergories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Catergory catergory = db.Catergories.Find(id);
+            db.Catergories.Remove(catergory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Bookshelf/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        [Key]//data annotation used to further describe how something should work - only one key per record
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+
+        [ForeignKey("Category")]
+        public int CatergoryID { get; set; }
+        public virtual Catergory Category { get; set; }
+    }
+}
